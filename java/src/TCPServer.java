b51f6062ea9a02f5b058851d1b5ed8fc4cb7b541
Rohleder <rohleder@protonmail.com>
2017-10-24 14:10:02 +0200

    working

--- java/src/TCPServer.java
+++ java/src/TCPServer.java
@@ -4,24 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 class TCPServer {
+    private static final int PORT = 6789;
     private static PathDatabase mDatabase;
 
     public static void main(String argv[])  {
-        int port = 6789;
         for(int i = 0; i<4; i++){
-            final int tempPort = port+i;
+            final int tempPort = PORT+i;
             Thread t = new Thread(() -> {
                 ServerSocket beaconSocket = null;
                 try {
                     beaconSocket = new ServerSocket(tempPort);
-                    Socket connectionSocketBeacon = beaconSocket.accept();
-                    BufferedReader inFromBeacon = new BufferedReader(new InputStreamReader(connectionSocketBeacon.getInputStream()));
-                    String beaconSentence = inFromBeacon.readLine();
-                    System.out.println("From " + connectionSocketBeacon.getInetAddress() + " : " + beaconSentence);
+                    Socket connectionSocket = beaconSocket.accept();
+
+                    while(true) {
+                        BufferedReader beaconReader = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                        String beaconSentence = beaconReader.readLine();
+                        System.out.println("From " + connectionSocket.getInetAddress() + " : " + beaconSentence);
+                    }
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
             });
             t.start();
         }
