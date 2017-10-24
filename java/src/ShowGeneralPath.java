baa662ee9103517a25bc421ac69e667d0ab9d2c7
Rohleder <rohleder@protonmail.com>
2017-10-24 18:38:52 +0200

    changed names

--- java/src/ShowGeneralPath.java
+++ java/src/ShowGeneralPath.java
@@ -28,45 +28,42 @@ public class ShowGeneralPath extends JApplet {
             setBackground(Color.gray);
             setSize(400, 200);
             generalPath = new Vector();
-            GeneralPath path1, path2,path3;
+            GeneralPath beaconPath1, beaconPath2,beaconPath3;
 
-            path1 = new GeneralPath();
-            path1.moveTo(25, 35);
-            path1.lineTo(35, 50);
-            path1.lineTo(50, 20);
-            path1.lineTo(80, 30);
-            path1.curveTo(20, 100, 110, 60, 40, 100);
-            generalPath.addElement(path1);
+            beaconPath1 = new GeneralPath();
+            beaconPath1.moveTo(25, 35);
+            beaconPath1.lineTo(35, 50);
+            beaconPath1.lineTo(50, 20);
+            beaconPath1.lineTo(80, 30);
+            beaconPath1.curveTo(20, 100, 110, 60, 40, 100);
+            generalPath.addElement(beaconPath1);
 
-            path2 = new GeneralPath();
-            path2.moveTo(120, 120);
-            path2.lineTo(120, 180);
-            path2.lineTo(180, 120);
-            path2.lineTo(180, 180);
-            path2.closePath();
-            generalPath.addElement(path2);
+            beaconPath2 = new GeneralPath();
+            beaconPath2.moveTo(120, 120);
+            beaconPath2.lineTo(120, 180);
+            beaconPath2.lineTo(180, 120);
+            beaconPath2.lineTo(180, 180);
+            beaconPath2.closePath();
+            generalPath.addElement(beaconPath2);
 
-            path3 = new GeneralPath();
-            path3.moveTo(250, 120);
-            path3.lineTo(70, 80);
-            path3.lineTo(220, 40);
-            path3.lineTo(280, 40);
-            path3.lineTo(230, 80);
-            path3.closePath();
-            generalPath.addElement(path3);
+            beaconPath3 = new GeneralPath();
+            beaconPath3.moveTo(250, 120);
+            beaconPath3.lineTo(70, 80);
+            beaconPath3.lineTo(220, 40);
+            beaconPath3.lineTo(280, 40);
+            beaconPath3.lineTo(230, 80);
+            beaconPath3.closePath();
+            generalPath.addElement(beaconPath3);
         }
         public void paint(Graphics g) {
-            int beaconSize = 20;
-
             Graphics2D g2d = (Graphics2D) g;
+            int beaconSize = 20;
             ((Graphics2D) g).translate(-(beaconSize/2),-(beaconSize/2));
             g.drawOval(canvas.getWidth()/10,canvas.getHeight()/(beaconSize/2),beaconSize,beaconSize);
             ((Graphics2D) g).translate(-(beaconSize/2),-(beaconSize/2));
             g.drawOval(canvas.getWidth()/10*9,canvas.getHeight()/(beaconSize/2)*9,beaconSize,beaconSize);
             g.drawOval(canvas.getWidth()/10,canvas.getHeight()/(beaconSize/2)*9,beaconSize,beaconSize);
             g.drawOval(canvas.getWidth()/10*9,canvas.getHeight()/(beaconSize/2),beaconSize,beaconSize);
-
-
 
             for (int i = 0; i < generalPath.size(); i++) {
                 g2d.setPaint(Color.white);
