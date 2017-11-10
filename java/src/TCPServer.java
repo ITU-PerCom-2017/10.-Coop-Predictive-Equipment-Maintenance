d6511b032437fc00633384e1b8692fc75ccb75be
Rohleder <rohleder@protonmail.com>
2017-11-10 17:27:10 +0100

    test

--- java/src/TCPServer.java
+++ java/src/TCPServer.java
@@ -23,7 +23,18 @@ class TCPServer {
             ServerSocket serverSocket = null;
 
             BufferedWriter bw = null;
+            PrintStream o = new PrintStream(new File("A.txt"));
 
+            // Store current System.out before assigning a new value
+            PrintStream console = System.out;
+
+            // Assign o to output stream
+            System.setOut(o);
+            System.out.println("This will be written to the text file");
+
+            // Use stored value for output stream
+            System.setOut(console);
+            System.out.println("This will be written on the console!");
 
             try {
                 serverSocket = new ServerSocket(port);
