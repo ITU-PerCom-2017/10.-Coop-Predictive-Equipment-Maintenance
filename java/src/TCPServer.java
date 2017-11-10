35cd6d210d73ac758375f0e27f8c92a36f888916
Rohleder <rohleder@protonmail.com>
2017-11-10 17:32:26 +0100

    test

--- java/src/TCPServer.java
+++ java/src/TCPServer.java
@@ -25,18 +25,18 @@ class TCPServer {
             BufferedWriter bw = null;
             try{
                 PrintStream o = new PrintStream(new File("A.txt"));
+                // Store current System.out before assigning a new value
+                PrintStream console = System.out;
 
+                // Assign o to output stream
+                System.setOut(o);
+                System.out.println("This will be written to the text file");
             }catch (FileNotFoundException e){
 
                 e.printStackTrace();
             }
 
-            // Store current System.out before assigning a new value
-            PrintStream console = System.out;
 
-            // Assign o to output stream
-            System.setOut(o);
-            System.out.println("This will be written to the text file");
 
             // Use stored value for output stream
             System.setOut(console);
