d07feda3875955eacc0acd85c996c932b93b0bf6
soea <soea@itu.dk>
2017-11-21 15:02:43 +0100

    udp + iterate through bluetooth

--- lopy/main.py
+++ lopy/main.py
@@ -10,9 +10,10 @@ import binascii
 from machine import Timer
 pycom.heartbeat(False)
 pycom.rgbled(0x000000) # turn off led
+port = 6791
 
-lopyID = 2 # set this unique to each lopy
-print('started! lopy id: ', lopyID)
+lopyID = 3 # set this unique to each lopy
+print('started! lopy id: ', lopyID, ' port: ', port)
 #for breaking out of while(True) loops. Removed when program is finished
 chrono = Timer.Chrono() #
 chrono.start()
@@ -20,10 +21,6 @@ chrono.start()
 wlan = WLAN(mode=WLAN.STA)
 nets = wlan.scan()
 
-bluetooth = Bluetooth()
-bluetooth.start_scan(-1)
-adv = None # bluetooth advertisement
-
 pycom.rgbled(0x7f0770)
 time.sleep(0.5)
 pycom.rgbled(0x000000) # turn off led
@@ -32,8 +29,21 @@ pycom.rgbled(0x7f0770)
 time.sleep(0.5)
 pycom.rgbled(0x000000) # turn off led
 time.sleep(1)
+pycom.rgbled(0x7f0770)
+time.sleep(0.5)
+pycom.rgbled(0x000000) # turn off led
+time.sleep(1)
+#pycom.rgbled(0x7f0770)
+#time.sleep(0.5)
+#pycom.rgbled(0x000000) # turn off led
+#time.sleep(1)
+#pycom.rgbled(0x7f0000) # red
+#time.sleep(0.5)
+#pycom.rgbled(0x000000) # turn off led
+#time.sleep(1)
 
-pycom.rgbled(0x7f0000)
+#pycom.rgbled(0x7f0000)
+
 for net in nets: # connect to rpi wifi
      if net.ssid == 'cooppifi':
          print('rpi found!')
@@ -47,59 +57,56 @@ for net in nets: # connect to rpi wifi
          else:
              print("could not connect to wifi, resetting lopy...")
              machine.reset()
-#pycom.rgbled(0x7f7f00) # yellow
 
-time.sleep(2)
+bluetooth = Bluetooth()
+bluetooth.start_scan(-1)
+adv = None # bluetooth advertisementa
+
+
+time.sleep(1)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_UDP)
+s.connect(socket.getaddrinfo('10.42.0.1', port)[0][-1])
 
 def sendToSocket(beaconID, rssi):
     RSSIpositive = rssi * -1
-    #newData = beaconID + lopyID + RSSIpositive
-    #newData = 255
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(socket.getaddrinfo('10.42.0.1', 6792)[0][-1])
-    pycom.rgbled(0x007f00)
-    time.sleep(0.5)
-    pycom.rgbled(0x000000) # turn off led
-    time.sleep(0.5)
-    print('.')
     data = bytearray()
-    #data.append(newData)
-    data.append(1)
-    data.append(2)
-    data.append(3)
-    print("sending to socket, binary: ", data, data[0])
-    #conv = binascii.hexlify(data)
-    #print(conv, encoding='utf8')
+    data.append(lopyID)
+    data.append(beaconID)
+    data.append(RSSIpositive)
     s.send(data)
-    s.close()
+
+# new mac adresses:
+#C7:C3:83:EE:AA:A4
+#FB:6F:11:25:79:9B
+#F7:F0:BB:A6:ED:F7
+#C3:C7:E7:73:7C:3F
 
 # b'f3bc0b6feb4c' # raw: b'\xf3\xbc\x0bo\xebL'
 # b'edbed48d0b87' # raw: b'\xed\xbe\xd4\x8d\x0b\x87
 # b'c7c383eeaaa4' # raw?
 # b'cd87e6a38dc2' # raw?
-print('!')
-while True:
-    pycom.rgbled(0x007f00) # green
-    adv = bluetooth.get_adv()
 
-    if adv != None:
-        #print('!!')
+#pycom.rgbled(0x007f00) # green
+while True:
+    pycom.rgbled(0x007f00)
+    advList = bluetooth.get_advertisements()
+    for adv in advList:
         macRaw = adv[0] # binary mac address
         macReal = binascii.hexlify(macRaw) # convert to hexadecimal
-        if macReal == b'f3bc0b6feb4c':
+        if macReal == b'fb6f1125799b': # old: b'f3bc0b6feb4c':
             print('beacon A rssi: ', adv[3]) # adv[3] is rssi
             sendToSocket(beaconID = 1, rssi = adv[3])
-        elif macReal == b'edbed48d0b87':
+        elif macReal == b'f7f0bba6edf7': # old: b'edbed48d0b87':
             print('beacon B rssi: ', adv[3])
             sendToSocket(beaconID = 2, rssi = adv[3])
-        elif macReal == b'c7c383eeaaa4':
+        elif macReal == b'c7c383eeaaa4': # <-- old
             print('beacon C rssi: ', adv[3])
             sendToSocket(beaconID = 3, rssi = adv[3])
-        elif macReal == b'cd87e6a38dc2':
+        elif macReal == b'c3c7e7737c3f': # old: b'cd87e6a38dc2':
             print('beacon D rssi: ', adv[3])
             sendToSocket(beaconID = 4, rssi = adv[3]) # adv[3]
     #time.sleep(1)
-
+s.close()
     #lap = chrono.read() # for breaking out of loop (remove this part when program is finished)
     #if lap > 30:
     #    print("breaking")
